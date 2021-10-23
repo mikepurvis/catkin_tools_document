c6e5a99b6418b1e7559da110dc06580dc48126e0
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-10-23 13:48:01 +0200

    Add write_file functionstage

--- catkin_tools_document/util.py
+++ catkin_tools_document/util.py
@@ -30,6 +30,25 @@ def which(program):
             return executable
 
 
+def write_file(logger, event_queue, contents: Any, dest_path: str, mode: str = 'w') -> int:
+    """
+    FunctionStage functor that writes the contents to a file.
+    In case the file exists, the file is overwritten.
+
+    :param logger:
+    :param event_queue:
+    :param contents: Contents to write
+    :param dest_path: File to which the contents should be written
+    :param mode: file mode (default: 'w')
+    :return: return code
+    """
+    mkdir_p(os.path.dirname(dest_path))
+    with open(dest_path, mode) as f:
+        f.write(contents)
+
+    return 0
+
+
 def yaml_dump_file(logger, event_queue, contents: Any, dest_path: str, dumper=yaml.SafeDumper) -> int:
     """
     FunctionStage functor that dumps the contents of an object, which is accepted by yaml dumper, to a file.
