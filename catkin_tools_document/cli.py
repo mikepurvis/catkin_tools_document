1f61917ca559aea4724859f06a2ccbb8c1d198e0
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-11-10 20:57:25 +0100

    (cli) apply black linting

--- catkin_tools_document/cli.py
+++ catkin_tools_document/cli.py
@@ -36,13 +36,14 @@ def main(opts):
             # Suppress warnings since this won't necessarily find all packages
             # in the workspace (it stops when it finds one package), and
             # relying on it for warnings could mislead people.
-            this_package = find_enclosing_package(
-                search_start_path=getcwd(),
-                ws_path=ws_path,
-                warnings=[])
+            this_package = find_enclosing_package(search_start_path=getcwd(), ws_path=ws_path, warnings=[])
         except InvalidPackage as ex:
-            sys.exit(clr("@{rf}Error:@| The file %s is an invalid package.xml file."
-                         " See below for details:\n\n%s" % (ex.package_path, ex.msg)))
+            sys.exit(
+                clr(
+                    "@{rf}Error:@| The file %s is an invalid package.xml file."
+                    " See below for details:\n\n%s" % (ex.package_path, ex.msg)
+                )
+            )
 
         # Handle context-based package building
         if opts.document_this:
@@ -50,7 +51,8 @@ def main(opts):
                 opts.packages += [this_package]
             else:
                 sys.exit(
-                    "[document] Error: In order to use --this, the current directory must be part of a catkin package.")
+                    "[document] Error: In order to use --this, the current directory must be part of a catkin package."
+                )
 
         # If --start--with was used without any packages and --this was specified, start with this package
         if opts.start_with_this:
@@ -58,12 +60,10 @@ def main(opts):
                 opts.start_with = this_package
             else:
                 sys.exit(
-                    "[document] Error: In order to use --this, the current directory must be part of a catkin package.")
+                    "[document] Error: In order to use --this, the current directory must be part of a catkin package."
+                )
 
-    job_server.initialize(
-        max_jobs=4,
-        max_load=None,
-        gnu_make_enabled=False)
+    job_server.initialize(max_jobs=4, max_load=None, gnu_make_enabled=False)
 
     return document_workspace(
         ctx,
@@ -78,52 +78,114 @@ def main(opts):
         limit_status_rate=opts.limit_status_rate,
         no_notify=opts.no_notify,
         continue_on_failure=opts.continue_on_failure,
-        summarize_build=opts.summarize)
+        summarize_build=opts.summarize,
+    )
 
 
 def prepare_arguments(parser):
     add_context_args(parser)
 
     # What packages to document
-    pkg_group = parser.add_argument_group('Packages', 'Control which packages get documented.')
+    pkg_group = parser.add_argument_group("Packages", "Control which packages get documented.")
     add = pkg_group.add_argument
-    add('packages', metavar='PKGNAME', nargs='*',
-        help='Workspace packages to document, package dependencies are documented as well unless --no-deps is used. '
-             'If no packages are given, then all the packages are documented.')
-    add('--this', dest='document_this', action='store_true', default=False,
-        help='Build the package containing the current working directory.')
-    add('--no-deps', action='store_true', default=False,
-        help='Only document specified packages, not their dependencies.')
+    add(
+        "packages",
+        metavar="PKGNAME",
+        nargs="*",
+        help="Workspace packages to document, package dependencies are documented as well unless --no-deps is used. "
+        "If no packages are given, then all the packages are documented.",
+    )
+    add(
+        "--this",
+        dest="document_this",
+        action="store_true",
+        default=False,
+        help="Build the package containing the current working directory.",
+    )
+    add(
+        "--no-deps",
+        action="store_true",
+        default=False,
+        help="Only document specified packages, not their dependencies.",
+    )
 
-    add('-j', '--jobs', default=None,
-        help='Maximum number of build jobs to be distributed across active packages. (default is cpu count)')
-    add('-p', '--parallel-packages', metavar='PACKAGE_JOBS', dest='parallel_jobs', default=None,
-        help='Maximum number of packages allowed to be built in parallel (default is cpu count)')
+    add(
+        "-j",
+        "--jobs",
+        default=None,
+        help="Maximum number of build jobs to be distributed across active packages. (default is cpu count)",
+    )
+    add(
+        "-p",
+        "--parallel-packages",
+        metavar="PACKAGE_JOBS",
+        dest="parallel_jobs",
+        default=None,
+        help="Maximum number of packages allowed to be built in parallel (default is cpu count)",
+    )
 
     start_with_group = pkg_group.add_mutually_exclusive_group()
     add = start_with_group.add_argument
-    add('--start-with', metavar='PKGNAME', type=str,
-        help='Build a given package and those which depend on it, skipping any before it.')
-    add('--start-with-this', action='store_true', default=False,
-        help='Similar to --start-with, starting with the package containing the current directory.')
+    add(
+        "--start-with",
+        metavar="PKGNAME",
+        type=str,
+        help="Build a given package and those which depend on it, skipping any before it.",
+    )
+    add(
+        "--start-with-this",
+        action="store_true",
+        default=False,
+        help="Similar to --start-with, starting with the package containing the current directory.",
+    )
 
     add = pkg_group.add_argument
-    add('--continue-on-failure', '-c', action='store_true', default=False,
-        help='Try to continue documenting packages whose dependencies documented successfully even if some other requested '
-             'packages fail to document.')
+    add(
+        "--continue-on-failure",
+        "-c",
+        action="store_true",
+        default=False,
+        help="Try to continue documenting packages whose dependencies documented successfully even if some other requested "
+        "packages fail to document.",
+    )
 
-    behavior_group = parser.add_argument_group('Interface', 'The behavior of the command-line interface.')
+    behavior_group = parser.add_argument_group("Interface", "The behavior of the command-line interface.")
     add = behavior_group.add_argument
-    add('--verbose', '-v', action='store_true', default=False,
-        help='Print output from commands in ordered blocks once the command finishes.')
-    add('--interleave-output', '-i', action='store_true', default=False,
-        help='Prevents ordering of command output when multiple commands are running at the same time.')
-    add('--no-status', action='store_true', default=False,
-        help='Suppresses status line, useful in situations where carriage return is not properly supported.')
-    add('--summarize', '--summary', '-s', action='store_true', default=None,
-        help='Adds a document summary to the end of a document; defaults to on with --continue-on-failure, off otherwise')
-    add('--no-summarize', '--no-summary', action='store_false', dest='summarize',
-        help='Explicitly disable the end of document summary')
+    add(
+        "--verbose",
+        "-v",
+        action="store_true",
+        default=False,
+        help="Print output from commands in ordered blocks once the command finishes.",
+    )
+    add(
+        "--interleave-output",
+        "-i",
+        action="store_true",
+        default=False,
+        help="Prevents ordering of command output when multiple commands are running at the same time.",
+    )
+    add(
+        "--no-status",
+        action="store_true",
+        default=False,
+        help="Suppresses status line, useful in situations where carriage return is not properly supported.",
+    )
+    add(
+        "--summarize",
+        "--summary",
+        "-s",
+        action="store_true",
+        default=None,
+        help="Adds a document summary to the end of a document; defaults to on with --continue-on-failure, off otherwise",
+    )
+    add(
+        "--no-summarize",
+        "--no-summary",
+        action="store_false",
+        dest="summarize",
+        help="Explicitly disable the end of document summary",
+    )
 
     def status_rate_type(rate):
         rate = float(rate)
@@ -131,10 +193,14 @@ def prepare_arguments(parser):
             raise ArgumentTypeError("must be greater than or equal to zero.")
         return rate
 
-    add('--limit-status-rate', '--status-rate', type=status_rate_type, default=10.0,
-        help='Limit the update rate of the status bar to this frequency. Zero means unlimited. '
-             'Must be positive, default is 10 Hz.')
-    add('--no-notify', action='store_true', default=False,
-        help='Suppresses system pop-up notification.')
+    add(
+        "--limit-status-rate",
+        "--status-rate",
+        type=status_rate_type,
+        default=10.0,
+        help="Limit the update rate of the status bar to this frequency. Zero means unlimited. "
+        "Must be positive, default is 10 Hz.",
+    )
+    add("--no-notify", action="store_true", default=False, help="Suppresses system pop-up notification.")
 
     return parser
